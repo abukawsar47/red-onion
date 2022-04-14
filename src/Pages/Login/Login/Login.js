2ddbdf8674efd33a4af9b2c5a02b2d204914851b
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-15 02:29:05 +0600

    Login and Register Page Design

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import logo from '../../../images/logo2.png'
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='container'>
+            <div className="w-50 mx-auto my-5">
+                <img className='mx-auto d-block mb-5' width={200} src={logo} alt="" />
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Email" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Button className='w-100 mb-3' variant="danger" type="submit">
+                        Login
+                    </Button>
+
+                    <Link to='/register' className='text-decoration-none text-danger text-center d-block fw-bold'>New to Red Onion? Please Register.</Link>
+                </Form>
+            </div>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import logo from '../../../images/logo2.png';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='container'>
+            <div className="w-50 mx-auto my-5 form-container">
+                <img className='mx-auto d-block mb-5' width={200} src={logo} alt="" />
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Control type="text" placeholder="Name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Email" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Confirm Password" required />
+                    </Form.Group>
+                    <Button className='w-100 mb-3' variant="danger" type="submit">
+                        Register
+                    </Button>
+                    <Link to='/login' className='text-decoration-none text-danger text-center d-block fw-bold'>Already have an account?</Link>
+                </Form>
+            </div>
         </div>
     );
 };
